b9f994880bc98c5f891e2f1d2a39fd09a21cecbe
NepyAnna <nepyann@gmail.com>
2025-04-10 12:40:30 +0200

    completed exercises

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ console.log('Hola soy tu consola y juntas vamos a aprender Javascript')
 
 //Escribe tu código aquí
 
-
+console.log('Bienvenida/o al bootcamp Femcoders de Factoría F5');
 
 
 //VARIABLES Y TIPOS DE DATOS
@@ -18,28 +18,31 @@ console.log('Hola soy tu consola y juntas vamos a aprender Javascript')
 
 //Escribe tu código aquí
 
-
-
+let myAge = 40;
+console.log(myAge);
 
 //Ejercicio 3: Crea una constante llamada postres con un tipo de dato array que contiene los siguientes elementos: helado, tarta, pastel e imprímelo en la consola. 
 
 //Escribe tu código aquí
 
-
+const desserts = ['ice cream', 'cake', 'pie'];
+console.log(desserts);
 
 
 //Ejercicio 4: Crea una constante con un tipo de dato objeto llamado coder que contengan dos propiedades, nombre y edad e imprímelo en la consola.
 
 //Escribe tu código aquí
 
-
-
+const coder =['Anna', 40];
+console.log(coder);
 
 //Ejercicio 5: Busca como imprimir en consola el tipo de dato de cada variable que hemos creado.
 
 //Escribe tu código aquí
 
-
+for(let index = 0; index < coder.length; index++){
+    console.log(typeof(coder[index]));
+}
 
 
 //OPERADORES ARITMÉTICOS
@@ -47,28 +50,30 @@ console.log('Hola soy tu consola y juntas vamos a aprender Javascript')
 
 //Escribe tu código aquí
 
-
-
+let sum = 3 + 4;
+console.log(sum);
 
 //Ejercicio 7: Crea una varible que reste 2 números e imprime el resultado en consola.
 
 //Escribe tu código aquí
 
-
+let dif = 8 - 4;
+console.log(dif);
 
 
 //Ejercicio 8: Crea una varible que multiplique 2 números e imprime el resultado en consola.
 
 //Escribe tu código aquí
 
-
-
+let multiplique = 5 * 5;
+console.log(multiplique);
 
 //Ejercicio 9: Crea una varible que divida 2 números e imprime el resultado en consola.
 
 //Escribe tu código aquí
 
-
+let div = 12 / 4;
+console.log(div);
 
 
 //OPERADORES DE COMPARACIÓN
@@ -76,14 +81,26 @@ console.log('Hola soy tu consola y juntas vamos a aprender Javascript')
 
 //Escribe tu código aquí
 
+let firstNum = 1;
+let secondNum = 2;
 
+console.log(firstNum < secondNum);
+console.log(firstNum > secondNum);
 
+if(firstNum < secondNum) {
+    console.log('firstNum less than secondNum');
+} else if(firstNum > secondNum) {
+    console.log('firstNum more than secondNum');
+} else {
+    console.log('firstNum equal secondNum');
+}
 
 // Ejercicio 10: Crea otra variable que compare las dos primeras variables creadas en el ejercicio anterior, para que el resultado impreso en la consola sea true.
 
 //Escribe tu código aquí
 
-
+let ifMoreThan = secondNum > firstNum;
+console.log(ifMoreThan);
 
 
 //Ejercicio 11: completa el ejercicio
@@ -94,6 +111,11 @@ let num2 = 20
 let comparacion = //Realiza la operación de comparación que consideres para que el resultado en consola sea true
 console.log(comparacion)
 */
+let num1 = 15
+let num2 = 20
+
+let comparacion = num1 < num2;
+console.log(comparacion)
 
 //Ejercicio 12: completa el ejercicio
 /*
@@ -103,6 +125,11 @@ let num3AsString = "1"
 let result = //Realiza la operación de comparación que consideres entre las dos variables dadas, para que el resultado en consola sea false sin usar el comparador de desigualdad
 console.log(result) 
 */
+let num3 = 1
+let num3AsString = "1"
+
+let result = num3===num3AsString;
+console.log(result)
 
 //Ejercicio 13: completa el ejercicio
 /*
@@ -110,15 +137,20 @@ let result2 = //Realiza la operación de comparación que consideres con las dos
 console.log(result2)
 */
 
+num3 = 1
+num3AsString = "1"
+let result2 = num3 == num3AsString
+console.log(result2)
 
 //OPERADORES DE CADENAS
 //Ejercicio 14: Crea una variable con tu nombre y otra variable con tu apellido y crea otra nueva variable con tu nombre completo concatenando las dos variables anteriores e imprime en consola. Debe haber un espacio entre tu nombre y apellido.
 
 //Escribe tu código aquí
+let myName = 'Anna';
+let surname = 'Nepyivoda';
 
-
-
-
+let fullname = myName + ' ' + surname;
+console.log(fullname);
 
 //OPERADORES DE LÓGICA
 //Ejercicio 15: Descomenta todo el bloque desde la variable 'a' hasta el 'console.log(res6)' y completa el ejercicio. (Saldrá un error y no podrás visualizar el resultado hasta que termines este ejercicio)
@@ -143,3 +175,23 @@ console.log(res5)
 
 let res6 = a == 6 __ b == 3 //Eliminar el espacio "__" y coloca el comparador lógico que consideres para que el resultado sea true
 console.log(res6) */
+let a = 6;
+let b = 3;
+
+let res =  a < 10 && b > 1
+console.log(res)
+
+let res2 = a < 10 && b < 1
+console.log(res2)
+
+let res3 = a == 5 || b == 5
+console.log(res3)
+
+let res4 = a == 6 || b == 0
+console.log(res4)
+
+let res5 = a == 0 || b == 3
+console.log(res5)
+
+let res6 = a == 6 && b == 3
+console.log(res6) 
